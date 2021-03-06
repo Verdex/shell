5d944102b50053898653120800773468b0e9d3df
David L. Inman <davidlevere@gmail.com>
2021-03-06 15:39:27 -0500

    add event loop in main.py

--- client/src/main.py
+++ client/src/main.py
@@ -2,8 +2,9 @@
 import pygame
 import resources.images
 
-pygame.init()
+EventLoopWait = 16
 
+pygame.init()
 
 window_style = 0
 best_depth = pygame.display.mode_ok((800, 800), window_style, 32)
@@ -11,14 +12,41 @@ screen = pygame.display.set_mode((800, 800), window_style, best_depth)
 
 images = resources.images.Images()
 
-screen.fill([255, 255, 255])
 
-x = pygame.Surface((32, 32))
-x.fill([255, 255, 255])
-x.blit(images.at_symbol.image, (0, 0))
+game_running = True
 
-screen.blit(x, (50, 50))
+while game_running:
 
-pygame.display.update()
+    loop_start = pygame.time.get_ticks()
 
-pygame.time.delay(5000)
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            pass
+        elif event.type == pygame.KEYUP:
+            pass
+        elif event.type == pygame.MOUSEBUTTONDOWN: 
+            pass
+        elif event.type == pygame.MOUSEBUTTONUP: 
+            pass
+        elif event.type == pygame.VIDEORESIZE:
+            pass
+        elif event.type == pygame.QUIT:
+            game_running = False
+
+    screen.fill([255, 255, 255])
+
+    x = pygame.Surface((32, 32))
+    x.fill([255, 255, 255])
+    x.blit(images.at_symbol.image, (0, 0))
+
+    screen.blit(x, (50, 50))
+
+    pygame.display.update()
+
+    loop_end = pygame.time.get_ticks()
+
+    loop_duration = loop_end - loop_start
+
+    if loop_duration < EventLoopWait:
+        print(loop_duration)
+        pygame.time.delay(EventLoopWait - loop_duration)
