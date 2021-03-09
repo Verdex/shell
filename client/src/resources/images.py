d596ba8a61b56acf5accd136831de82f98196ed6
David L. Inman <davidlevere@gmail.com>
2021-03-08 21:52:58 -0500

    add id to images

--- client/src/resources/images.py
+++ client/src/resources/images.py
@@ -3,12 +3,12 @@ from resources.util import load_image
 import resources.sizes as sizes
 
 class Image:
-    def __init__(self, name, size, image):
+    def __init__(self, id, name, size, image):
+        self.id = id
         self.name = name
         self.size = size
         self.image = image
 
 class Images:
-
     def __init__(self):
-        self.at_symbol = Image("at_symbol", sizes.Normal, load_image("at_symbol.png"))
+        self.at_symbol = Image(0, "at_symbol", sizes.Normal, load_image("at_symbol.png"))
