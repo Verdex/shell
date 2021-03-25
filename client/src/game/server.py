54f69fe3714e1bb1843178d3c0cc7b768c0fba1a
David L. Inman <davidlevere@gmail.com>
2021-03-24 23:03:55 -0400

    struct

--- client/src/game/server.py
+++ client/src/game/server.py
@@ -2,35 +2,17 @@
 import socket
 import array
 import sys
+import struct
 
 ServerAddress = "127.0.0.1"
 ServerPort = 3145 
 
 VersionZero = 0
 ConnectRequestType = 0
-ListType = 1
 
-def parse(bs):
-    version = bs[0]
-    if version == VersionZero:
-        t = bs[1]    
-        if t == ConnectRequestType:
-            pass
-        elif t == ListType:
-            pass
-        else:
-            raise SystemExit(f"Unknown server response type: {t}")
-    else:
-        raise SystemExit(f"Unknown server response version: {t}")
-
-def concat(items):
-    ret = array.array('B') 
-    for item in items:
-        ret.frombytes(item)
-    return ret.tobytes()
 
 def create_connect_request():
-    return concat([byte(VersionZero), byte(ConnectRequestType)])
+    return struct.pack('BB', VersionZero, ConnectRequestType) 
     
 class Server:
 
